be1b8ddc8e01fc96931231660c7acc9358d63861
Richard DeVaul <richard.devaul@gmail.com>
2020-08-03 17:41:22 -0700

    added support for the pretty printing of vector, line, and poly objects. NOTE: for vector, line, and poly -- these are implicitly defined as lists with appropriate structure, not python 3 classes. added support for barycentric coordinates (2D) and inside testing for triangles and polygons in the x-y plane.

--- geom.py
+++ geom.py
@@ -107,6 +107,43 @@ def mag(a):
 def dist(a,b):  # compute distance between two points a & b
     return mag(sub(a,b))
 
+## R^3 -> bool functions
+## ---------------------
+
+# utility function to determine if a list of points lies in one of the
+# cardinal planes: XY, YZ, XZ
+
+def isCardinalPlanar(plane="xy",points=[]):
+    if plane=="xy":
+        idx = 2
+    elif plane == "yz":
+        idx = 0
+    elif plane == "xz":
+        idx = 1
+    else:
+        raise ValueError('bad plane specification: {}'.format(plane))
+        return False
+
+    if len(points) < 2:
+        return True
+
+    ref = points[0][idx]
+    for i in range(1,len(points)):
+        if abs(points[i][idx]-ref) > epsilon:
+            return False
+    return True
+
+## conveniience functions
+def isXYPlanar(points=[]):
+    return isCardinalPlanar("xy",points)
+
+def isYZPlanar(points=[]):
+    return isCardinalPlanar("yz",points)
+
+def isXZPlanar(points=[]):
+    return isCardinalPlanar("xz",points)
+
+
 ## R^4 -> R functions 
 ## ----------------------------------------
 def dot4(a,b):
@@ -118,9 +155,51 @@ def mag4(a):
 def dist4(a,b):  # compute distance between two points a & b
     return mag(sub4(a,b))
 
+
+## misc operations
+## -----------------------------------------
+
+# pretty printing string formatter for vectors, lines, and polygons.
+# You can use this anywhere you use str(), since it will fall back to
+# str() if the argument isn't a yapCAD vector, line, or polygon.
+def vstr(a):
+    # utility functions for recursively checking and formatting lists
+    def _isallvect(foo):
+        if len(foo)==1:
+            return isvect(foo[0])
+        else:
+            return isvect(foo[0]) and _isallvect(foo[1:])
+    def _makestr(foo):
+        if len(foo) ==1:
+            return vstr(foo[0])
+        else:
+            return vstr(foo[0]) + ", " + _makestr(foo[1:])
+    # if it's not a list, it's not a vector, line, or poly    
+    if not isinstance(a,list):
+        return str(a) # fall back to default string formatting
+    # if it is a vector, format it to leave out extraneous
+    # coordinates.  NOTE: this means that 3 vectors that happen to
+    # fall into the z=0 plane will be formatted as though they were 2
+    # vectors.
+    if isvect(a):
+        if abs(a[3]-1.0) > epsilon: # not in w=1
+            return "[{}, {}, {}, {}]".format(a[0],a[1],a[2],a[3])
+        elif abs(a[2]) > epsilon: # not in z=0
+            return "[{}, {}, {}]".format(a[0],a[1],a[2])
+        else: # in x-y plane
+            return "[{}, {}]".format(a[0],a[1])
+    # if it is a list of vectors (line or poly) format it appropriately
+    elif len(a)>0 and _isallvect(a):
+        return "["+_makestr(a)+"]"
+    else: #it's none of those things, fall back to default string
+          #formatting
+        return str(a)
+    
+
 ## operations on lines
 ## --------------------
-## lines are defined as lists of points, i.e. [[x1, y1],[x2, y2]]
+## lines are defined as lists of two points, i.e.
+## [vect(x1, y1),vect(x2, y2)]
 
 ## Compute the intersection of two lines that lie in the same x,y plane
 def intersectXY(l1,l2,inside=True):
@@ -192,8 +271,11 @@ def linePointXY(l,p,inside=True,distance=False):
     z2=b[2]
 
     ## check to see if all three points lie in the same x,y plane
-    if abs(z1-z0) > epsilon or abs(z2-z0) > epsilon:
-        return False
+    if not isXYPlanar([p,a,b]):
+        raise ValueError('non-XY points in linePointXY call')
+        return false
+    # if abs(z1-z0) > epsilon or abs(z2-z0) > epsilon:
+    #    return False
 
     linedist = abs( ((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)/abdist)
 
@@ -282,47 +364,203 @@ def linePointXY(l,p,inside=True,distance=False):
 def linePointXYDist(l,p,inside=True):
     return linePointXY(l,p,inside,distance=True)
     
+## functions that compute barycentric coordinates for 2D triangles and
+## 3D tetrahedral simplexes
+
+## given a point a and three vertices, all of which fall into the x-y
+## plane, compute the barycentric coordinates lam1, lam2, and lam3
+def barycentricXY(a,p1,p2,p3):
+    if not isXYPlanar([a,p1,p2,p3]):
+        raise ValueError('non-XY points in barycentricXY call')
+    
+    x=a[0]
+    y=a[1]
+
+    x1=p1[0]
+    y1=p1[1]
+
+    x2=p2[0]
+    y2=p2[1]
+
+    x3=p3[0]
+    y3=p3[1]
+
+    denom=(y2-y3)*(x1-x3) + (x3-x2)*(y1-y3)
+
+    if abs(denom) < epsilon:
+        raise ValueError('degenerate triangle in barycentricXY call')
+    
+    lam1 = ((y2-y3)*(x-x3) + (x3-x2)*(y-y3))/denom
+    lam2 = ((y3-y1)*(x-x3) + (x1-x3)*(y-y3))/denom
+    lam3 = 1.0-(lam1+lam2)
+
+    return [lam1,lam2,lam3]
+
+## given a triangle defined as a list of three points and a test
+## point, determine if that test point lies inside or outside the
+## triangle, assuming that all points lie in the x-y plane
+
+def _isinsidetriangleXY(a,poly): #no value checking version
+    p1=poly[0]
+    p2=poly[1]
+    p3=poly[2]
+    bary = barycentricXY(a,p1,p2,p3)
+    if bary[0] >= 0.0 and bary[0] <= 1.0 and \
+       bary[1] >= 0.0 and bary[1] <= 1.0 and \
+       bary[2] >= 0.0 and bary[2] <= 1.0:
+        return True
+    return False
+        
+
+# 'safe' value-check version
+def isinsidetriangleXY(a,poly):
+    if len(poly) != 3 or not isXYPlanar(poly + [a ]):
+        raise ValueError('bad poly length or non-XY points in insidetriangleXY call')
+    return _isinsidetriangleXY(a,poly)
+
+## given a polygon defined as a list of three or more points and a
+## test point, determine if that test point lies inside or ouside the
+## polygon, assuming that all points lie in the x-y plane
+
+def _isinsidepolyXY(a,poly): #no value checking version
+    if len(poly) == 3:
+        return _isinsidetriangleXY(a,poly)
+    else:
+        return _isinsidetriangleXY(a,poly[0:3]) or \
+               _isinsidepolyXY(a,poly[1:len(poly)])
+
+# 'safe' value-check version
+def isinsidepolyXY(a,poly):
+    if len(poly) < 3 or not isXYPlanar(poly + [ a]):
+        raise ValueError('bad poly length or non-XY points in insidepolyXY call')
+    return _isinsidepolyXY(a,poly)
 
 ## check to see if we have been invoked on the command line
 ## if so, run some tests
+
 if __name__ == "__main__":
     a = vect(5,0)
     b = vect(0,5)
     c = vect(-3,-3)
     d = vect(1,1)
-
+    e = vect(10,10)
+    
     print("light-weight unit tests for geom.py module")
-    print("some points: " + str(a) + ", "
-          + str(b) + ", " + str(c) + ", " + str(d))
+    print("some points: a:" + vstr(a) + ", b:"
+          + vstr(b) + ", c:" + vstr(c) + ", d:" + vstr(d) + ", e:" + vstr(e))
 
     print("mag a: " + str(mag(a)))
-    print("add(a,b): " + str(add(a,b)))
-    print("sub(a,b): " + str(sub(a,b)))
+    print("add(a,b): " + vstr(add(a,b)))
+    print("sub(a,b): " + vstr(sub(a,b)))
     print("mag(sub(a,b)): " + str(mag(sub(a,b))))
     print("mag(sub(a,b)) == sqrt(50): " + str(mag(sub(a,b))==sqrt(50.0)))
     
     l1 = [a,b]
     l2 = [c,d]
-
-    print("l1: " + str(l1) + ", l2: " + str(l2) )
+    l3 = [c,e]
+    
+    print("l1:" + vstr(l1) + ", l2:" + vstr(l2) +", l3:" + vstr(l3))
 
     int0 = intersectXY(l1,l1)
     int1 = intersectXY(l1,l2,False)
     int2 = intersectXY(l1,l2,True)
+    int3 = intersectXY(l1,l3,True)
 
-    print("intersectXY(l1,l1): " + str(int0))
-    print("intersectXY(l1,l2,False): " + str(int1))
-    print("intersectXY(l1,l2,True): " + str(int2))
+    print("expect False: intersectXY(l1,l1): " + vstr(int0))
+    print("expect [2.5, 2.5]: intersectXY(l1,l2,False): " + vstr(int1))
+    print("expect False: intersectXY(l1,l2,True): " + vstr(int2))
+    print("expect [2.5, 2.5]: intersectXY(l1,l3,True): " + vstr(int3))
     
     print("linePointXY(l1,vect(0,0)): "
-          + str(linePointXY(l1,vect(0,0))))
+          + vstr(linePointXY(l1,vect(0,0))))
     print("linePointXYDist(l1,vect(0,0)) == sqrt(12.5): "
-          + str(abs(linePointXYDist(l1,vect(0,0))-sqrt(12.5))<epsilon))
+          + vstr(abs(linePointXYDist(l1,vect(0,0))-sqrt(12.5))<epsilon))
     print("linePointXY(l1,vect(0,10),False): "
-          + str(linePointXY(l1,vect(vect(0,10)),False)))
+          + vstr(linePointXY(l1,vect(vect(0,10)),False)))
     print("linePointXY(l1,vect(0,10),True): "
-          + str(linePointXY(l1,vect(0,10),True)))
+          + vstr(linePointXY(l1,vect(0,10),True)))
     print("linePointXY(l1,vect(10,0),False): "
-          + str(linePointXY(l1,vect(10,0),False)))
+          + vstr(linePointXY(l1,vect(10,0),False)))
     print("linePointXY(l1,vect(10,0),True): "
-          + str(linePointXY(l1,vect(10,0),True)))
+          + vstr(linePointXY(l1,vect(10,0),True)))
+
+    print("do some planar point checking")
+    # points in the x-y plane
+    p1 = vect(2.5,0)
+    p2 = vect(5,5)
+    p3 = vect(0,5)
+    p4 = vect(2.5,10)
+
+    # points in the x,z plane
+    p5 = vect(1,0,-5)
+    p6 = vect(10,0,10)
+    
+    # points in the y-z plane
+    p7 = vect(0,2.-1)
+    p8 = vect(0,10,10)
+
+    print('expect True: isCardinalPlanar("xy",[p1,p2,p3,p4]) : {}'.format(
+        isCardinalPlanar("xy",[p1,p2,p3,p4])))
+    print('expect False: isCardinalPlanar("xz",[p1,p2,p3,p4]) : {}'.format(
+        isCardinalPlanar("xz",[p1,p2,p3,p4])))
+    print('expect False: isCardinalPlanar("yz",[p1,p2,p3,p4]) : {}'.format(
+        isCardinalPlanar("yz",[p1,p2,p3,p4])))
+
+    print('expect True: isCardinalPlanar("xz",[p1,p5,p6]) : {}'.format(
+        isCardinalPlanar("xz",[p1,p5,p6])))
+
+    print('expect True: isCardinalPlanar("yz",[p2,p7,p8]) : {}'.format(
+        isCardinalPlanar("yz",[p2,p7,p8])))
+
+    try:
+        print('deliberate bad plane specification, should raise ValueError')
+        print('isCardinalPlanar("FOO!",[p1,p2,p3,p4]) : {}'.format(
+            isCardinalPlanar("FOO!",[p1,p2,p3,p4])))
+    except ValueError as err:
+        print('got expected result:',err)
+
+    print("polygon inside testing")
+    tri1 = [p1,p2,p3]
+    poly1 = [p1,p2,p4,p3]
+    p = vect(2.5,1.0)
+    q = vect(2.5,5.0)
+    r = vect(2.5,7.0)
+    s= vect(-10,-10)
+    t= vect(10,10)
+    print ("p: {}, q: {}, r: {}, s: {}, t: {}".format(vstr(p), vstr(q),
+                                                      vstr(r), vstr(s),
+                                                      vstr(t)))
+    print ("tri1: {}".format(vstr(tri1)))
+    print("expect True: isinsidetriangleXY(p,tri1): {}"\
+          .format(isinsidetriangleXY(p,tri1)))
+    print("expect True: isinsidetriangleXY(q,tri1): {}"\
+          .format(isinsidetriangleXY(q,tri1)))
+    print("expect False: isinsidetriangleXY(r,tri1): {}"\
+          .format(isinsidetriangleXY(r,tri1)))
+    print("expect False: isinsidetriangleXY(s,tri1): {}"\
+          .format(isinsidetriangleXY(s,tri1)))
+    print("expect False: isinsidetriangleXY(t,tri1): {}"\
+          .format(isinsidetriangleXY(t,tri1)))
+    print("inside poly testing")
+    print ("tri1: {}".format(vstr(tri1)))
+    print ("poly1: {}".format(vstr(poly1)))
+    print("expect True: isinsidepolyXY(p,tri1): {}"\
+          .format(isinsidepolyXY(p,tri1)))
+    print("expect True: isinsidepolyXY(q,tri1): {}"\
+          .format(isinsidepolyXY(q,tri1)))
+    print("expect False: isinsidepolyXY(r,tri1): {}"\
+          .format(isinsidepolyXY(r,tri1)))
+    
+    print("expect True: isinsidepolyXY(p,poly1): {}"\
+          .format(isinsidepolyXY(p,poly1)))
+    print("expect True: isinsidepolyXY(q,poly1): {}"\
+          .format(isinsidepolyXY(q,poly1)))
+    print("expect True: isinsidepolyXY(r,poly1): {}"\
+          .format(isinsidepolyXY(r,poly1)))
+    print("expect False: isinsidepolyXY(s,tri1): {}"\
+          .format(isinsidepolyXY(s,tri1)))
+    print("expect False: isinsidepolyXY(t,tri1): {}"\
+          .format(isinsidepolyXY(t,tri1)))
+    
+    print("done!")
+
