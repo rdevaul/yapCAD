6aab1dd22a50ef1d6b9b952106e9318563c524ca
Richard DeVaul <richard.devaul@gmail.com>
2020-08-05 17:55:57 -0700

    added line-arc intersection example

--- example2.py
+++ example2.py
@@ -1,6 +1,7 @@
 ## First computational geometry example for yapCAD
 
 from geom import *
+print("yapCAD 2D computational geometry examples")
 
 # define some points
 a = vect(5,0)
@@ -14,4 +15,21 @@ l2 = [c,d]
 
 # calculate the intersection of l1 and l2
 int0 = intersectXY(l1,l2,True)
-print("intersection of {} and {}: {}".format(l1,l2,int0))
+
+# define a semicircular arc centerd at 2.5, 2,5 with a radius of 2.5
+# extending from 90 degress to 135 degrees
+
+arc1=[vect(2.5,2.5),vect(2.5,90.0,270.0)]
+
+# calculate the intersection of the line l1 and the arc arc1
+
+int1 = lineArcIntersectXY(l1,arc1,True)
+
+# print the results
+print("line l1:",vstr(l1))
+print("line l2:",vstr(l2))      
+print("arc arc1:",vstr(arc1))
+
+print("intersection of l1 and l2:", vstr(int0))
+print("intersection of arc1 and l1:",vstr(int1))
+
