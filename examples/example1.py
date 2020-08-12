701e11154dff3bfd673b49eab9e0c407e3bd2ea3
Richard DeVaul <richard.devaul@gmail.com>
2020-08-12 14:42:56 -0700

    updates to more recent yapCAD API

--- examples/example1.py
+++ examples/example1.py
@@ -1,30 +1,33 @@
 ## First drawing example for yapCAD
-print("yapCAD DXF drawing example")
+print("example1.py -- yapCAD DXF drawing example")
 
 from ezdxf_drawable import *
 from geom import *
 
 #set up DXF rendering
 drawable=ezdxfDraw()
-drawable.saveas("example1-out")
+filename="example1-out"
+print("\nOutput file name is {}.dxf".format(filename))
+drawable.saveas(filename)
+
 
 ## make dxf-renderable geometry
 
 # make a point located at 10,10 in the x-y plane, rendered as a small
 # cross and circle
-point=Point(vect(10,10),"xo")
+p=Point(point(10,10),"xo")
 
 # make a line segment between the points -5,10 and 10,-5 in the x-y plane
-line=Line(vect(-5,10),
-          vect(10,-5))
+l=Line(point(-5,10),
+       point(10,-5))
 
 # make an arc with a center at 0,3 with a radius of 3, from 45 degrees
 # to 135 degrees
-arc=Arc(vect(0,3),3,45,135)
+a=Arc(point(0,3),3,45,135)
 
-point.draw()
-line.draw()
-arc.draw()
+p.draw()
+l.draw()
+a.draw()
 
 # write out the geometry as example1-out.dxf
 drawable.display()
