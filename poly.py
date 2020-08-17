ea8f8c2cd2f921782538509d2a191e8fa088d96f
Richard DeVaul <richard.devaul@gmail.com>
2020-08-17 14:43:02 -0700

    make Polyline and Polygon subclasses of Geometry

--- poly.py
+++ poly.py
@@ -1,5 +1,6 @@
 # pure geometry polyline and polygon support for yapCAD
 from geom import *
+from geometry import *
 
 ## The goal is to describe open and closed multi-element geometric
 ## figures that are specified in terms of point, line, and arc
@@ -15,11 +16,11 @@ from geom import *
 
 
 
-class Polyline:
+class Polyline(SampleGeometry):
     """Generalized multi-element open figure class"""
 
     def __init__(self,a=False):
-        self._elements=[]
+        self._elem=[]
         self._length=0.0
         self._lengths=[]
         self._lines=[]
@@ -29,23 +30,23 @@ class Polyline:
         self._bbox=line(point(-epsilon,-epsilon),
                         point(epsilon,epsilon))
         if isinstance(a,Polyline):
-            self._elements = deepcopy(a._elements)
+            self._elem = deepcopy(a._elem)
             self._updateInternals()
         elif isinstance(a,list):
             for i in a:
                 if ispoint(i): 
-                    self._elements.append(i)
+                    self._elem.append(i)
                 else:
                     raise ValueError("bad argument to Polygon constructor")
 
     def __repr__(self):
-        return 'Polyline({})'.format(vstr(self._elements))
+        return 'Polyline({})'.format(vstr(self._elem))
 
     ## add another drawing element
     def addPoint(self,element):
         if ispoint(element):
             self._update=True  # flag that we need to recalculate stuff
-            self._elements.append(element)
+            self._elem.append(element)
         else:
             raise ValueError('attempt to add a non point to Polyline')
 
@@ -53,17 +54,17 @@ class Polyline:
     def setPoint(self,i,p):
         if not ispoint(p):
             raise ValueError('bad point passed to Polyline.setPoint(): '.format(p))
-        if i < len(self._elements):
-            self._elements[i]=point(p)
-        elif i == len(self._elements):
-            self._elements.append(p)
+        if i < len(self._elem):
+            self._elem[i]=point(p)
+        elif i == len(self._elem):
+            self._elem.append(p)
         else:
             raise ValueError('index out of range in PolylinesetPoint(): '.format(i))
         self._update=True
         
-    ## return a copy of the elements list
-    def getElements(self):
-        return deepcopy(self._elements)
+    ## return a copy of the elem list
+    def getElem(self):
+        return deepcopy(self._elem)
     
     def _updateInternals(self):
         if self._update:
@@ -76,23 +77,23 @@ class Polyline:
     ## points.  If last and first points are the same, ignore the
     ## last point.
     def _updateCenter(self):
-        l = len(self._elements)
+        l = len(self._elem)
         if l == 0:
             return
         elif l == 1:
-            self._center = center(self._elements[0]) # center is sole point
+            self._center = center(self._elem[0]) # center is sole point
             e = point(epsilon,epsilon)            # make bounding box
             self._bbox = line(sub(self._center,e),
                              add(self._center,e))
         else:
             
-            if dist(center(self._elements[0]),
-                    center(self._elements[-1])) < epsilon:
+            if dist(center(self._elem[0]),
+                    center(self._elem[-1])) < epsilon:
                 l -= 1
 
             p = point(0,0,0)
             for i in range(l):
-                p = add(center(self._elements[i]),p)
+                p = add(center(self._elem[i]),p)
 
             self._center = scale(p,1/l)
         return
@@ -109,20 +110,20 @@ class Polyline:
             self._updateInternals()
         return self._length
     
-    ## function to take the points in the elements[] list and build a
+    ## function to take the points in the elem[] list and build a
     ## list of the individual lines, line lengths and total length to
     ## facilitate sampling
     
     def _updateLines(self):
-        for i in range(1,len(self._elements)):
-            p0=  center(self._elements[i-1])
-            p1=  center(self._elements[i])
+        for i in range(1,len(self._elem)):
+            p0=  center(self._elem[i-1])
+            p1=  center(self._elem[i])
             self._lines.append(line(p0,p1))
             l = dist(p0,p1)
             self._lengths.append(l)
             self._length += l
-        if len(self._elements) > 2 and dist(center(self._elements[0]),
-                                            center(self._elements[-1])) < epsilon:
+        if len(self._elem) > 2 and dist(center(self._elem[0]),
+                                            center(self._elem[-1])) < epsilon:
             self._closed = True
 
     def sample(self,u):
@@ -150,7 +151,7 @@ class Polygon(Polyline):
         super().__init__(a)
         self.outline=[]
 
-        elm = self._elements
+        elm = self._elem
 
         l = len(elm)
         if l > 2:
@@ -160,15 +161,15 @@ class Polygon(Polyline):
                dist(elm[0],elm[-1]) < epsilon or \
                iscircle(elm[0]) and iscircle(elm[-1]) and \
                abs(elm[0][1][0] - elm[-1][1][0]) < epsilon:
-                self._elements.pop()
+                self._elem.pop()
         
     def __repr__(self):
-        return 'Polygon({})'.format(vstr(self._elements))
+        return 'Polygon({})'.format(vstr(self._elem))
 
     ## add another drawing element
     def addLine(self,element):
         if isline(element):
-            self._elements.append(element)
+            self._elem.append(element)
         else:
             raise ValueError('attempt to add a non point, line or arc to poly')
     
@@ -177,15 +178,15 @@ class Polygon(Polyline):
         if isarc(element):
             ## mark the psuedovector with a w=-1 tag to indicate that
             ## this is an arc, not a line
-            self._elements.append(element)
+            self._elem.append(element)
         else:
             raise ValueError('attempt to add a non point, line or arc to poly')
 
     ## compute barycentric center of figure by equally weighting the
-    ## center of all of the elements
+    ## center of all of the elem
     def center(self):
         p = point(0,0)
-        for e in self._elements:
+        for e in self._elem:
             if ispoint(e):
                 p = add(p,e)
             elif isline(e):
@@ -195,19 +196,19 @@ class Polygon(Polyline):
             elif isarc(e):
                 p = add(p,arccenter(e))
             else:
-                raise ValueError('bad element in poly elements')
-        if len(self._elements) > 0:
-            p = scale(p,1.0/len(self._elements))
+                raise ValueError('bad element in poly elem')
+        if len(self._elem) > 0:
+            p = scale(p,1.0/len(self._elem))
         return p
                 
     def remove(self,element):
-        self._elements.remove(element)
+        self._elem.remove(element)
                 
-    ## function to take the elements in the elements[] list and
+    ## function to take the elements in the elem[] list and
     ## construct the full outline.  For example, consider a list of
-    ## three points in the elements[] list -- this would result in the
+    ## three points in the elem[] list -- this would result in the
     ## construction of three lines that results in a closed curve.
-    ## Similarly, three circles in the elements list would result in a
+    ## Similarly, three circles in the elem list would result in a
     ## "rounded triangle" composed of the circle-tangent lines joined
     ## by three arcs.  Elements that are explicitly specified lines or
     ## non-circular arcs are joined to adjacent elements by lines
@@ -261,12 +262,12 @@ class Polygon(Polyline):
         self._length=0
         self._lengths=[]
 
-        if len(self._elements) < 2:
+        if len(self._elem) < 2:
             ## if one element, the outline is the element
-            if len(self._elements) == 1:
-                self.outline=deepcopy(self._elements)
+            if len(self._elem) == 1:
+                self.outline=deepcopy(self._elem)
                 self._calclength()
-            ## if there are fewer than one elements, there is nothing to do
+            ## if there are fewer than one elem, there is nothing to do
             return
         ## we have work to do.  Construct all lines and circle-tangent
         ## lines first, inserting explicit line and arc elements.
@@ -274,16 +275,16 @@ class Polygon(Polyline):
         ## total length and length list for use in sample() function.
 
         
-        for i in range(len(self._elements)):
+        for i in range(len(self._elem)):
             if i == 0:
-                e0 = self._elements[-1]
+                e0 = self._elem[-1]
             else:
-                e0 = self._elements[i-1]
-            e1 = self._elements[i]
-            if i == len(self._elements)-1: #last item
-                e2 = self._elements[0]
+                e0 = self._elem[i-1]
+            e1 = self._elem[i]
+            if i == len(self._elem)-1: #last item
+                e2 = self._elem[0]
             else:
-                e2 = self._elements[i+1]
+                e2 = self._elem[i+1]
             ## work through element types
             if isvect(e0): #e0 is a point
                 _fromPointAdd(e0,e1,e2)
@@ -323,8 +324,9 @@ class Polygon(Polyline):
                 pp0 = lineArcIntersectXY(e0,e1,False)
                 pp1 = lineArcIntersectXY(e2,e1,False)
                 # these should be tangent lines, so exactly one
-                # intersection each.  If not, we have a problem
-                if len(pp0) != 1 or len(pp1) != 1:
+                # intersection each.  sometimes we get two that are
+                # just a bit more than epsilon apart, which is OK.
+                if len(pp0) < 1 or len(pp1) < 1:
                     raise ValueError('bad line-circle intersection in poly outline calculation')
                 # we are assuming that elements are ordered in a
                 # counter-clockwise fashion
