88498f0ff2d9e97889540ba954b046ed36c201f6
Richard DeVaul <richard.devaul@gmail.com>
2020-09-07 16:23:16 -0700

    added support for colors, layers, and other drawing attributes.  Added support for text

--- ezdxf_drawable.py
+++ ezdxf_drawable.py
@@ -7,26 +7,96 @@ import drawable
 
 ## base class to provide dxf drawing functionality
 class ezdxfDraw(drawable.Drawable):
-    doc = ezdxf.new(dxfversion='R2010')
-    doc.layers.new('DEFAULT', dxfattribs={'color': 2})
-    msp = doc.modelspace()
-    filename = "yapCAD-out"
 
     def __init__(self):
+        self.doc = ezdxf.new(dxfversion='R2010',setup=True)
+        self.doc.layers.new('PATHS',  dxfattribs={'color': 7}) #white
+        self.doc.layers.new('DRILLS',  dxfattribs={'color': 4}) #aqua
+        self.doc.layers.new('DOCUMENTATION', dxfattribs={'color': 2}) #yellow
+        self.layerlist = [False, '0','PATHS','DRILLS','DOCUMENTATION']
+        self.linetypelist = [ False,'Continuous']
+        self.msp = self.doc.modelspace()
+        self.filename = "yapCAD-out"
+
+        self.layer=False        # no layer set, use '0'
+        self.color=False    # default is 256 = autocad BYLAYER
+        self.linetype=False # default is 'Continuous'
+        
         super().__init__()
 
     def __repr__(self):
         return 'an instance of ezdxfDraw'
+
+    def layerset(self,layer=False):
+        if not layer in self.layerlist:
+            raise ValueError('bad layer passed to layerset: {}'.format(layer))
+        self.layer=layer
+        
+    def colorset(self,color=False):
+        if not color in range(257):
+            raise ValueError('bad color in colorset: {}'.format(color))
+        self.color=color
+
+    def linetypeset(self,linetype=False):
+        if not linetype in self.linetypelist:
+            raise ValueError('bad linetype in linetypeset: {}'.format(linetype))
+        self.linetype=linetype
         
     def draw_line(self,p1,p2):
-        self.msp.add_line((p1[0], p1[1]), (p2[0], p2[1]))
+        layer=self.layer
+        if layer == False:
+            layer = '0'
+        color = self.color
+        if color == False:
+            color = 256 # bylaer
+        linetype = self.linetype
+        if linetype == False:
+            linetype = 'Continuous'
+            
+        self.msp.add_line((p1[0], p1[1]), (p2[0], p2[1]),
+                          dxfattribs={'layer': layer,
+                                      'color': color,
+                                      'linetype': linetype})
+            
 
     def draw_arc(self,p,r,start,end):
-        if start==0 and end==360:
-            self.msp.add_circle((p[0],p[1]),r)
-        else:
-            self.msp.add_arc((p[0],p[1]),r,start,end)
+        layer=self.layer
+        if layer == False:
+            layer = '0'
+        color = self.color
+        if color == False:
+            color = 256 # bylaer
+        linetype = self.linetype
+        if linetype == False:
+            linetype = 'Continuous'
 
+        if start==0 and end==360:
+            self.msp.add_circle((p[0],p[1]),r,
+                          dxfattribs={'layer': layer,
+                                      'color': color,
+                                      'linetype': linetype})
+        else:
+            self.msp.add_arc((p[0],p[1]),r,start,end,
+                          dxfattribs={'layer': layer,
+                                      'color': color,
+                                      'linetype': linetype})
+
+    def draw_text(self,text,location,
+                  align='LEFT',
+                  attr={'style': 'LiberationMono',
+                        'height': 1.0}):
+        layer=self.layer
+        if layer == False:
+            layer = '0'
+        color = self.color
+        if color == False:
+            color = 256 # bylaer
+        dxfattr = dict(attr)
+        dxfattr.update({ 'layer': layer,
+                         'color': color})
+        self.msp.add_text(text,dxfattr).set_pos((location[0],location[1]),
+                                             align=align)
+            
     def saveas(self,name):
         self.filename = name
         
