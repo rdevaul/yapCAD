6affd0c387266f9cf41d885543fcd3c52ab184b2
Richard DeVaul <richard.devaul@gmail.com>
2020-09-18 22:58:32 -0700

    first working implementation for union operation

--- yapcad/combine.py
+++ yapcad/combine.py
@@ -23,6 +23,58 @@ class Boolean(IntersectGeometry):
         bbox2 = g2.bbox()
         inter = intersectXY(g1.geom(),g2.geom(),params=True)
 
+        ## utility to perform combination on one "segment"
+        def cmbin(g1,g2,itr):
+            g1s = itr[0][0]
+            g1e = itr[0][1]
+            g2s = itr[1][0]
+            g2e = itr[1][1]
+
+            seg = []
+            if g1e < g1s:
+                g1e+=1.0
+            if g2e < g2s:
+                g2e+=1.0
+                
+            p1=g1.sample(((g1s+g1e)/2)%1.0)
+            p2=g2.sample(((g2s+g2e)/2)%1.0)
+            if self._type == 'union':
+                if g2.inside(p1):
+                    seg = g1.segment(g1e,g1s)
+                else:
+                    seg = g1.segment(g1s,g1e)                    
+            if self._type == 'intersection':
+                prin("intersection not tested")
+                if g2.inside(p1):
+                    seg = g1.segment(g1s,g1e)
+                else:
+                    seg = g2.segment(g2s,g2e)
+            if self._type == 'difference':
+                print("difference not tested")
+                if g2.inside(p1):
+                    seg = g1.segment(g1s,g1e)
+                else:
+                    seg = g1.segment(g1e,g1s)                    
+            return seg
+
+        ## argument alternation for cmbin
+        def swapcmbin(g1,g2,itr):
+            return cmbin(g2,g1,[itr[1],itr[0]])
+
+        ## utility function to sort intersections into non-decreasing
+        ## order
+        def rsort(il):
+            nl = []
+            rl = []
+            rr = []
+            for i in range(len(il[0])):
+                nl.append([il[0][i],il[1][i]])
+            nl.sort(key=lambda x: x[0])
+            for i in range(len(nl)):
+                rl.append(nl[i][0])
+                rr.append(nl[i][1])
+            return [rl,rr]
+
         if inter == False: # disjoint, but one could be inside the other
             if isinsidebbox(bbox1,bbox2[0]) and isinsidebbox(bbox1,bbox2[1]):
                 ## g2 is inside g1
@@ -36,30 +88,20 @@ class Boolean(IntersectGeometry):
         ## single point of intersection: return a geometry list with both
             return [g1, g2]
         ## There are two or more points of intersection.
-        if len(inter[0]) == 2:
-            ## for g1, we have two possiblities for the "outside"
-            ## segment: u1 --> u2 or u2 --> u1.  Likewise, the same is
-            ## true for g2.  If we sample a point (u1+u2)/2 on g1 and
-            ## it falls outside g2, then that is the segment to
-            ## choose.  Likewise for the other.
-            g1s = inter[0][0]
-            g1e = inter[0][1]
-            g2s = inter[1][0]
-            g2e = inter[1][1]
-
-            p=g1.sample((g1s+g1e)/2)
-            if g2.inside(p):
-                g1s = g1e
-                g1e = inter[0][0]
-
-            p=g2.sample((g2s+g2e)/2)
-            if g1.inside(p):
-                g2s = g1e
-                g2e = inter[1][0]
-
-            seg1 = g1.segment(g1s,g1e)
-            seg2 = g2.segment(g2s,g2e)
-            return seg1 + seg2
+        inter = rsort(inter)
+        if len(inter[0]) % 2 == 0:
+            r = []
+            for i in range(1,len(inter[0])+1):
+                f = cmbin
+                if i%2 == 1:
+                    f = swapcmbin
+                r += f(g1,g2,[[inter[0][i-1],
+                               inter[0][i%len(inter[0])]],
+                              [inter[1][i-1],
+                               inter[1][i%len(inter[0])]]])
+            return r
+        else:
+            raise ValueError('odd number of intersections -- bailing')
             
     def geom(self):
         if self._type == 'union' and len(self._elem)==2:
