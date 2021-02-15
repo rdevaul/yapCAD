68a2b13d21a1d0f2b9607a4e9c4146eed13ebced
Richard DeVaul <richard.devaul@gmail.com>
2021-02-14 20:13:48 -0800

    major extensions and revisions to support animated objects and a materials library

--- src/yapcad/pyglet_drawable.py
+++ src/yapcad/pyglet_drawable.py
@@ -53,8 +53,148 @@ yapCAD_legend="""
 </font>
 """
 
+class Material():
+    """
+    Representation of an OpenGL-style material
+    """
+
+    def __repr__(self):
+        return f"Material(ambient={self.ambient}, diffuse={self.diffuse}, specular={self.specular}, emission={self.emission}, shininess={self.shininess}, desc=\"{self.desc}\")"
+
+    def __checkargs(self,v):
+        if ( not (len(v) == 3 or len(v) == 4) or
+             len(list(filter(lambda x: not isinstance(x,(int,float)),v))) > 0 or
+             len(list(filter(lambda x: x < 0.0 or x > 1.0,v))) > 0):
+            raise ValueError('bad arguments to property setter')
+        if len(v) == 3:
+            return [v[0],v[1],v[2],1.0]
+        else:
+            return v
+
+    
+    def __init__(self,**kwargs):
+        self.__ambient = vec(0.2, 0.2, 0.2, 1.0)
+        self.__diffuse = vec(0.5, 0.5, 0.5, 1.0)
+        self.__specular = vec(0.5, 0.5, 0.5, 1.0)
+        self.__emission = vec(0.0, 0.0, 0.0, 1.0)
+        self.__shininess = 50
+        self.__desc = ""
+        if 'ambient' in kwargs:
+            self.ambient = kwargs['ambient']
+        if 'diffuse' in kwargs:
+            self.diffuse = kwargs['diffuse']
+        if 'specular' in kwargs:
+            self.specular = kwargs['specular']
+        if 'emission' in kwargs:
+            self.emission = kwargs['emission']
+        if 'shininess' in kwargs:
+            self.shininess = kwargs['shininess']
+        if 'desc' in kwargs:
+            self.desc = kwargs['desc']
+
+    @property
+    def ambient(self):
+        return list(self.__ambient)
+
+    @ambient.setter
+    def ambient(self,*v):
+        if len(v) == 1:
+            v = v[0]
+        v = self.__checkargs(v)
+        self.__ambient = vec(*v)
+        
+    @property
+    def diffuse(self):
+        return list(self.__diffuse)
+
+    @diffuse.setter
+    def diffuse(self,*v):
+        if len(v) == 1:
+            v = v[0]
+        v = self.__checkargs(v)
+        self.__diffuse = vec(*v)
+        
+    @property
+    def specular(self):
+        return list(self.__specular)
+
+    @specular.setter
+    def specular(self,*v):
+        if len(v) == 1:
+            v = v[0]
+        v = self.__checkargs(v)
+        self.__specular = vec(*v)
+
+    @property
+    def emission(self):
+        return list(self.__emission)
+
+    @emission.setter
+    def emission(self,*v):
+        if len(v) == 1:
+            v = v[0]
+        v = self.__checkargs(v)
+        self.__emission = vec(*v)
+
+    @property
+    def shininess(self):
+        return self.__shininess
+
+    @shininess.setter
+    def shininess(self,v):
+        if not isinstance(v,(int,float)) or v < 0 or v > 128:
+            raise ValueError('bad shininess')
+        self.__shininess=v
+
+    @property
+    def desc(self):
+        return self.__desc
+
+    @desc.setter
+    def desc(self,v):
+        if not isinstance(v,str):
+            raise ValueError('bad description')
+        self.__desc = v
+
+    @property
+    def material(self):
+        return pyglet.model.Material(self.desc,
+                                     self.diffuse,
+                                     self.ambient,
+                                     self.specular,
+                                     self.emission,
+                                     self.shininess)
+
+        
+
+## global materials dictionary
+materials = {}
+
+materials['default'] = Material(ambient=[0.5, 0.3, 0.2, 1.0],
+                                diffuse = [0.6, 0.5, 0.3, 1.0],
+                                specular = [1.0, 0.8, 0.5, 1.0],
+                                emission = [0.0, 0.0, 0.0, 1.0],
+                                shininess = 20,
+                                desc = "default material, dull gold")
+
+
+materials['groundplane'] = Material(diffuse = [0.02, 0.02, 0.023, 1.0],
+                                    ambient = [0.01, 0.01, 0.01, 1.0],
+                                    specular = [0.05, 0.05, 0.07, 1.0],
+                                    emission = [0.0, 0.0, 0.0, 1.0],
+                                    shininess = 10,
+                                    desc = "ground plane material")
+
+## "throwaway" class for keeping track of object stuff
+class GeomObject:
+    pass
+
+        
 ## class to provide openGL drawing functionality
 class pygletDraw(drawable.Drawable):
+    """
+    yapCAD ``drawable`` subclass for OpenGL rendering with pyglet
+    """
 
     def window(self):
         window = []
@@ -84,23 +224,14 @@ class pygletDraw(drawable.Drawable):
         gl.glLightfv(gl.GL_LIGHT1, gl.GL_DIFFUSE, vec(0.8, 0.8, 0.8, 1))
         gl.glLightfv(gl.GL_LIGHT1, gl.GL_POSITION, vec(-10.0, -20.0, 20.0, 0))
 
-        # Create a Material and Group for the Model
-        diffuse = [0.5, 0.5, 0.3, 1.0]
-        ambient = [0.5, 0.5, 0.3, 1.0]
-        specular = [1.0, 1.0, 1.0, 1.0]
-        emission = [0.0, 0.0, 0.0, 1.0]
-        shininess = 50
-        material = pyglet.model.Material("", diffuse, ambient, specular, emission, shininess)
+        # Create a default material and Group for the Model
+        material = materials['default'].material
         self.group = pyglet.model.MaterialGroup(material=material)
 
         # Create a Material and Group for the ground plane
-        diffuse = [0.02, 0.02, 0.023, 1.0]
-        ambient = [0.01, 0.01, 0.01, 1.0]
-        specular = [0.05, 0.05, 0.07, 1.0]
-        emission = [0.0, 0.0, 0.0, 1.0]
-        shininess = 10
-        material2 = pyglet.model.Material("ground", diffuse, ambient, specular, emission, shininess)
+        material2 = materials['groundplane'].material
         self.group2 = pyglet.model.MaterialGroup(material=material2)
+
 
 
     def makeBatches(self):
@@ -113,8 +244,9 @@ class pygletDraw(drawable.Drawable):
         epsM = point(-epsilon,-epsilon,-epsilon)
 
         ## utility funtion to update bounding box based on a list of points
-        def upbb(pp,box):
+        def upbb(pp,box,offset=[0,0,0,1]):
             for p in pp:
+                p = add(p,offset)
                 if box:
                     box = [ [min(box[0][0],p[0]),min(box[0][1],p[1]),min(box[0][2],p[2])],
                             [max(box[1][0],p[0]),max(box[1][1],p[1]),max(box[1][2],p[2])] ]
@@ -156,22 +288,72 @@ class pygletDraw(drawable.Drawable):
                                     self.group,
                                     li,ll,lc)
 
+            
+            def addSurface(s,batch,group,offset,bbx):
+                assert s[0] == 'surface'
+                vert = s[1]
+                norm = s[2]
+                ind = s[3]
+                pp = []
+                ln = int(len(vert)/3)
+                for i in range(ln):
+                    pp.append(vert[i*3:(i+1)*3])
+                bbx = upbb(pp,bbx,offset)
+                batch.add_indexed(int(len(vert)/3),
+                                  gl.GL_TRIANGLES,
+                                  group,
+                                  ind,
+                                  ('v3f/static', vert),
+                                  ('n3f/static', norm))
+                return bbx
+
+            def lineSurface(s,obj):
+                assert s[0] == 'surface'
+                vert = s[1]
+                ind = s[3]
+                drawn = []
+                for j in range(0,len(ind),3):
+                    i = ind[j:(j+3)]
+                    l1 = i[0],i[1]
+                    if l1[0] > l1[1]:
+                        l1 = i[1],i[0]
+                    l2 = i[1],i[2]
+                    if l2[0] > l2[1]:
+                        l2 = i[2],i[1]
+                    l3 = i[2],i[0]
+                    if l3[0] > l3[1]:
+                        l3 = i[0],i[2]
+                    if not l1 in drawn:
+                        self.draw_line(vert[l1[0]*3:l1[0]*3+3],
+                                       vert[l1[1]*3:l1[1]*3+3],
+                                       entity=obj)
+                        drawn.append(l1)
+                    if not l2 in drawn:
+                        self.draw_line(vert[l2[0]*3:l2[0]*3+3],
+                                       vert[l2[1]*3:l2[1]*3+3],
+                                       entity=obj)
+                        drawn.append(l2)
+                    if not l3 in drawn:
+                        self.draw_line(vert[l3[0]*3:l3[0]*3+3],
+                                       vert[l3[1]*3:l3[1]*3+3],
+                                       entity=obj)
+                        drawn.append(l3)
+                for l in obj.lines:
+                    ll = l[0]
+                    lc = l[1]
+                    obj.linebatch.add(2,gl.GL_LINES,self.group,ll,lc)
+
+                    
+        for o in self.__objectdict.values():
+            for s in o.surfaces:
+                bbx = addSurface(s,o.batch,o.group,
+                                 point(o.x,o.y,o.z),bbx)
+                lineSurface(s,o)
+                
+
         for s in self.__surfaces:
-            assert s[0] == 'surface'
-            vert = s[1]
-            norm = s[2]
-            ind = s[3]
-            pp = []
-            ln = int(len(vert)/3)
-            for i in range(ln):
-                pp.append(vert[i*3:(i+1)*3])
-            bbx = upbb(pp,bbx)
-            self.__batch2.add_indexed(int(len(vert)/3),
-                                    gl.GL_TRIANGLES,
-                                    self.group,
-                                    ind,
-                                    ('v3f/static', vert),
-                                    ('n3f/static', norm))
+            bbx = addSurface(s,self.__batch2,self.group,
+                             point(0,0,0),bbx)
 
         ## Create a ground plane
         self.__batch3.add_indexed(4,
@@ -203,23 +385,30 @@ class pygletDraw(drawable.Drawable):
         self.layerlist = [False, '0','PATHS','DRILLS','DOCUMENTATION']
 
         def on_key_press(symbol,modifiers):
+            handled = False
             if symbol == pyglet.window.key.UP:
+                handled = True
                 self.__cameradist *= 1.1
                 if self.__cameradist > self.__maxcameradist:
                     self.__cameradist = self.__maxcameradist
             elif symbol == pyglet.window.key.DOWN:
+                handled = True
                 self.__cameradist *= .90909090
                 if self.__cameradist < self.__mincameradist:
                     self.__cameradist = self.__mincameradist
             elif symbol == pyglet.window.key.RETURN:
+                handled = True
                 self.__cameradist = self.camerastartdist
                 self.__rx = 0
                 self.__ry = 0
             elif symbol == pyglet.window.key.P:
+                handled = True
                 self.__drawground = not self.__drawground
             elif symbol == pyglet.window.key.M:
+                handled = True
                 self.__legend = not self.__legend
             elif symbol == pyglet.window.key.L:
+                handled = True
                 if not (self.__light0 or self.__light1):
                     self.__light0 = True
                     gl.glEnable(gl.GL_LIGHT0)
@@ -230,6 +419,10 @@ class pygletDraw(drawable.Drawable):
                     gl.glEnable(gl.GL_LIGHT1)
                 elif self.__light0 and self.__light1:
                     self.__light0 = self.__light1 = False
+            if handled:
+                return pyglet.event.EVENT_HANDLED
+            else:
+                return
             
                     
         self.__window = self.window()
@@ -254,8 +447,9 @@ class pygletDraw(drawable.Drawable):
         self.__surfaces=[]
         self.__labels = []
         self.__batch1 = graphics.Batch() # use for lines, points, etc.
-        self.__batch2 = graphics.Batch() # use for surfaces
+        self.__batch2 = graphics.Batch() # default, use for surfaces
         self.__batch3 = graphics.Batch() # use for environmental features, e.g. ground plane
+        self.__objectdict = {} # use for animated objects
         self.__bbox = False
 
         @self.__window.event
@@ -263,8 +457,6 @@ class pygletDraw(drawable.Drawable):
             self.__rx += dx
             self.__ry -= dy
             return pyglet.event.EVENT_HANDLED
-
-
 
         @self.__window.event
         def on_draw():
@@ -301,6 +493,21 @@ class pygletDraw(drawable.Drawable):
             if self.__light0 or self.__light1:
                 gl.glEnable(gl.GL_LIGHTING)
                 self.__batch2.draw()
+
+            for obj in self.__objectdict.values():
+                gl.glMatrixMode(gl.GL_MODELVIEW)
+                gl.glPushMatrix()
+                gl.glTranslatef(obj.x,obj.y,obj.z)
+                gl.glRotatef(obj.rx, 1, 0, 0)
+                gl.glRotatef(obj.ry, 0, 1, 0)
+                gl.glRotatef(obj.rz, 0, 0, 1)
+                if obj.lighting and (self.__light0 or self.__light1):
+                    gl.glEnable(gl.GL_LIGHTING)
+                    obj.batch.draw()
+                else:
+                    gl.glDisable(gl.GL_LIGHTING)
+                    obj.linebatch.draw()
+                gl.glPopMatrix()
 
             #gl.glDisable(gl.GL_BLEND)
             gl.glDisable(gl.GL_LIGHTING)
@@ -374,6 +581,10 @@ class pygletDraw(drawable.Drawable):
             dist = self.__mincameradist
         self._set_cameradist(dist)
 
+    @property
+    def objectdict(self):
+        return self.__objectdict
+
     
     ## Overload virtual yapcad.drawable base class dawing methods
     
@@ -383,11 +594,16 @@ class pygletDraw(drawable.Drawable):
         super().draw_point(p)
         self.__arcres=ar
 
-    def draw_line(self,p1,p2):
-        color = self.thing2color(self.linecolor,'f')
-        self.__lines.append([ ('v3f',(p1[0],p1[1],p1[2],
-                                      p2[0],p2[1],p2[2])),
-                              ('c3f',tuple(color + color)) ])
+    def draw_line(self,p1,p2,entity=None):
+        if not entity:
+            entity=self
+            elist = entity.__lines
+        else:
+            elist = entity.lines
+        color = self.thing2color(entity.linecolor,'f')
+        elist.append([ ('v3f',(p1[0],p1[1],p1[2],
+                               p2[0],p2[1],p2[2])),
+                       ('c3f',tuple(color + color)) ])
             
     def draw_arc(self,p,r,start,end):
         res = self.__arcres
@@ -481,9 +697,68 @@ class pygletDraw(drawable.Drawable):
         self.__linestrips.append([ ('v3f', tuple(p2)),\
                                    ('c3f',tuple(c2)),
                                    tuple(i2) ])
-            
 
-    def draw_surface(self,points,normals=None,faces=None):
+    def make_object(self,name,**kwargs):
+        if name in self.__objectdict.keys():
+            raise ValueError('duplicate object key')
+        obj = GeomObject()
+
+        if 'lighting' in kwargs:
+            if kwargs['lighting']:
+                obj.lighting = True
+            else:
+                obj.lighting=False
+        if 'position' in kwargs:
+            x = kwargs['position']
+            if not ispoint(x):
+                raise ValueError('bad position')
+            obj.x = x[0]
+            obj.y = x[1]
+            obj.z = x[2]
+        else:
+            obj.x = 0
+            obj.y = 0
+            obj.z = 0
+        if 'rotation' in kwargs:
+            r = kwargs['rotation']
+            obj.rx = r[0]
+            obj.ry = r[1]
+            obj.rz = r[2]
+        else:
+            obj.rx = 0
+            obj.ry = 0
+            obj.rz = 0
+        if 'material' in kwargs:
+            mat = kwargs['material']
+            if mat in materials:
+                obj.material = materials[mat]
+                m = obj.material.material
+                obj.group = pyglet.model.MaterialGroup(material=m)
+            else:
+                raise ValueError('unknown material')
+        else:
+            obj.material=materials['default']
+            obj.group = self.group
+
+        if 'animate' in kwargs:
+            pyglet.clock.schedule_interval(kwargs['animate'],1/60)
+
+        if 'linecolor' in kwargs:
+            obj.linecolor = kwargs['linecolor']
+        else:
+            obj.linecolor = 'gray'
+
+        obj.batch = graphics.Batch()
+        obj.linebatch = graphics.Batch()
+        obj.surfaces = []
+        obj.lines=[]
+        
+
+        self.__objectdict[name] = obj
+        
+        
+
+    def draw_surface(self,points,normals=None,faces=None,name='default'):
         if not (normals or faces):
             normals = points[2]
             faces = points[3]
@@ -497,7 +772,11 @@ class pygletDraw(drawable.Drawable):
             nrms+=n[0:3]
         for f in faces:
             inds+=f[0:3]
-        self.__surfaces.append(['surface',vrts,nrms,inds])    
+        if name == 'default':
+            self.__surfaces.append(['surface',vrts,nrms,inds])
+        else:
+            obj = self.__objectdict[name]
+            obj.surfaces.append(['surface',vrts,nrms,inds])
 
     ## overload base-class virtual display method
     def display(self):
