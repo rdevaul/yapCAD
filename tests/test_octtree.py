da2348da97104adb391ee76a887f4d596c01d003
Richard DeVaul <richard.devaul@gmail.com>
2025-10-06 11:31:26 -0700

    Update test_octtree.py
    fixed pyglet import statement

--- tests/test_octtree.py
+++ tests/test_octtree.py
@@ -7,7 +7,7 @@ from yapcad.octtree import *
 # Control flag for visual tests - set via environment variable or directly
 VISUALTEST = os.environ.get('VISUALTEST', 'false').lower() in ('true', '1', 'yes')
 if VISUALTEST:
-    from yapcad.pyglet_drawable import *
+    from yapcad.pyglet_drawable import pygletDraw
 
 def three2two(x):
     """take a 3D bounding box with non-zero z coordinates, return
